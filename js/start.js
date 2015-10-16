d3877c58f1753d05fb084c72376a20d36713f3c6
Juan Andres Segreda Johanning <andres@sailorpro.com>
2015-10-16 11:10:58 -0600

    trying image cache

--- js/start.js
+++ js/start.js
@@ -1,15 +1,18 @@
 
 function onDeviceReady(){
 	
-	try{
+	try{		
 		onDeviceReady_fm();
 		onDeviceReady_pn();
+		onDeviceReady_ic();
 		document.addEventListener("backbutton", backKeyDown, true);
 	    document.addEventListener("menubutton", menuKeyDown, true);
 		document.addEventListener("resume", onResume, false);
 		document.addEventListener("pause", onPause, false);
+		
 
 	}catch(e){
+		console.log(e);
 	}
 	setTimeout(function(){ $(".wConteiner div p").show();},3000);
 	
